1c8ea2b18b37f394344a80e682e46ce7cc5232a0
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-10-04 16:30:50 +0900

    5-4

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
 // 2022-10-04
 // 1916513 
 // main.c
-// chapter 05-3
+// chapter 05-4
 
 #include <stdio.h>
 
 int main(void){
-	int num =0;
-	char c; 
-	printf("문자열을 입력하세요: ");
-	while ( (c = getchar() ) != '\n' ){
-		if ((int)c>=48 && (int)c<=57) {
-			
-			num ++;
-		}
-	}
-	printf("숫자의 개수는 %d개입니다.",num);
+	int num;
+	int result=0;
+	 
+	printf("숫자를 입력하세요: ");
+	scanf("%d", &num);
+	for (int i =1; i<= num; i++){
+		result += i;
+	} 
+	printf("1부터 %d까지의 합은 %d 입니다.",num,result);
 }
