79f873028234d85a4e1e0265505bd8afadef453b
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-10-04 16:46:46 +0900

    5-6

--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 // 2022-10-04
 // 1916513 
 // main.c
-// chapter 05-5
+// chapter 05-6
 
 #include <stdio.h>
 
 int main(void){
-	int num1, num2;
-	char op;
-	printf("수식을  입력하세요: ");
-	scanf("%d %c %d", &num1,&op, &num2);
+	int answer = 12;
+	int num;
+	int tryNum = 0;
 	
-	if(op == '+') printf("%d %c %d = %d", num1, op, num2, num1+num2);
-	else if(op == '-') printf("%d %c %d = %d", num1, op, num2, num1-num2);
-	else if(op == '*') printf("%d %c %d = %d", num1, op, num2, num1*num2);
-	else if(op == '/') printf("%d %c %d = %d", num1, op, num2, num1/num2);
-	else printf("잘못입력하셨습니다.");
-
+	do {
+		tryNum += 1;
+		printf("숫자를 입력하세요: ");
+		scanf("%d",&num);
+		if (num > answer) printf("숫자가 정답보다 큽니다.\n");
+		else if (num < answer) printf("숫자가 정답보다 작습니다.\n");
+	}while(num!=answer);
+	
+	printf("%d회 시도 끝에 정답을 맞추었습니다!", tryNum); 
+	
 }
