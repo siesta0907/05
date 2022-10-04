45b64884bb3c392b1566dca74732ad304087839f
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-10-04 16:25:57 +0900

    5-3

--- main.c
+++ main.c
@@ -1,16 +1,19 @@
 // 2022-10-04
 // 1916513 
 // main.c
-// chapter 05-2
+// chapter 05-3
 
 #include <stdio.h>
 
 int main(void){
-	int num;
-	printf("정수 하나를 입력하시오" );
-	scanf("%d", &num);
-	if (num > 0) printf("절대값은 %d입니다.\n", num);
-	else printf("절대값은 %d입니다.\n", num*-1);
-	
-	
+	int num =0;
+	char c; 
+	printf("문자열을 입력하세요: ");
+	while ( (c = getchar() ) != '\n' ){
+		if ((int)c>=48 && (int)c<=57) {
+			
+			num ++;
+		}
+	}
+	printf("숫자의 개수는 %d개입니다.",num);
 }
