51a7b6432ad5bd1958985ccf7241226a7e71d0c4
siesta0907 <50662414+siesta0907@users.noreply.github.com>
2022-10-04 16:41:09 +0900

    5-5

--- main.c
+++ main.c
@@ -1,18 +1,20 @@
 // 2022-10-04
 // 1916513 
 // main.c
-// chapter 05-4
+// chapter 05-5
 
 #include <stdio.h>
 
 int main(void){
-	int num;
-	int result=0;
-	 
-	printf("숫자를 입력하세요: ");
-	scanf("%d", &num);
-	for (int i =1; i<= num; i++){
-		result += i;
-	} 
-	printf("1부터 %d까지의 합은 %d 입니다.",num,result);
+	int num1, num2;
+	char op;
+	printf("수식을  입력하세요: ");
+	scanf("%d %c %d", &num1,&op, &num2);
+	
+	if(op == '+') printf("%d %c %d = %d", num1, op, num2, num1+num2);
+	else if(op == '-') printf("%d %c %d = %d", num1, op, num2, num1-num2);
+	else if(op == '*') printf("%d %c %d = %d", num1, op, num2, num1*num2);
+	else if(op == '/') printf("%d %c %d = %d", num1, op, num2, num1/num2);
+	else printf("잘못입력하셨습니다.");
+
 }
